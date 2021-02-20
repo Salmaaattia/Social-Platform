5da7a20138175679acdd9d0c4c861f04a2589e70
Salmaaattia <you@example.com>
2021-02-20 23:22:19 +0200

    add basic views and paths

--- socialplatform/social_platform/urls.py
+++ socialplatform/social_platform/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('register', views.registeration, name='register'),
+    path('login', views.login, name='login'),
+    path('logout', views.logout, name='logout'),
+    path('post', views.post, name='post')
 ]
--- socialplatform/social_platform/views.py
+++ socialplatform/social_platform/views.py
@@ -6,3 +6,19 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the socialplatform index.")
+
+
+def registeration(request):
+    return HttpResponse("please enter your email and password")
+
+
+def login(request):
+    return HttpResponse("please login")
+
+
+def logout(request):
+    return HttpResponse("logout")
+
+
+def post(request):
+    return HttpResponse("post")
