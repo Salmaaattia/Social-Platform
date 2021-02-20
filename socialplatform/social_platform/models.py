b2aa01a94400a9fe572aef46312d7a9ee2954ed6
Salmaaattia <you@example.com>
2021-02-20 23:08:59 +0200

    create core models for app

--- socialplatform/social_platform/models.py
+++ socialplatform/social_platform/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=50)
+    birth_date = models.DateField()
+    posts_count = models.IntegerField()
+    posts = models.CharField(max_length=30)
+
+
+class Post(models.Model):
+    content = models.CharField(max_length=250)
+    date_posted = models.DateField()
+    owner =  models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    content = models.CharField(max_length=150)
+    date_commented = models.DateField()
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+
+class Like(models.Model):
+    owner = models.ForeignKey(User)
+    post = models.ForeignKey(Post)
