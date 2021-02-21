8d57d2cbb846a733e78ae34a648f03d1f3d81cf2
Salmaaattia <you@example.com>
2021-02-21 21:44:31 +0200

    comment out class User and add predefined one

--- socialplatform/main_app/models.py
+++ socialplatform/main_app/models.py
@@ -1,18 +1,22 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 
-# Create your models here.
-class User(models.Model):
-    name = models.CharField(max_length=50)
-    birth_date = models.DateField()
-    posts_count = models.IntegerField()
-    posts = models.CharField(max_length=30)
+# class User(models.Model):
+#     name = models.CharField(max_length=50)
+#     birth_date = models.DateField()
+#     posts_count = models.IntegerField()
+#     posts = models.CharField(max_length=30)
 
 
 class Post(models.Model):
-    content = models.CharField(max_length=250)
-    date_posted = models.DateField()
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.content
 
 
 class Comment(models.Model):
@@ -21,7 +25,13 @@ class Comment(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.owner
+
 
 class Like(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.owner
