b856c4f4ee79d31806b0c5049b9e0b4ceba0bb83
Salmaaattia <you@example.com>
2021-02-21 21:43:59 +0200

    change index name to home

--- socialplatform/main_app/urls.py
+++ socialplatform/main_app/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.home, name='home'),
     path('register', views.register, name='home-register'),
     path('login', views.login, name='login'),
     path('logout', views.logout, name='logout'),
--- socialplatform/main_app/views.py
+++ socialplatform/main_app/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from .models import Post
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the socialplatform index.")
-
+def home(request):
+    return render(request, 'main_app/home.html', {'posts':Post.objects.all()})
 
 def register(request):
     return render(request, 'main_app/register.html')
