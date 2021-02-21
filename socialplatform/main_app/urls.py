0248b9398828ed0996fab76bb58a314823cf2511
Salmaaattia <you@example.com>
2021-02-21 14:29:49 +0200

    merge to main

--- socialplatform/main_app/urls.py
+++ socialplatform/main_app/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('register', views.register, name='register'),
+    path('register', views.register, name='home-register'),
     path('login', views.login, name='login'),
     path('logout', views.logout, name='logout'),
     path('post', views.post, name='post')
