a0e0d80fde0ea359fbb129f0499fa6caf8bcedc1
Salmaaattia <you@example.com>
2021-02-21 21:40:07 +0200

    create users app

--- socialplatform/socialplatform/settings.py
+++ socialplatform/socialplatform/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
     'main_app.apps.MainAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
