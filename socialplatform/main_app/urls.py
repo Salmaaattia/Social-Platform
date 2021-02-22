7cd3d99a367161f81ab1c865550e5e738b4c5dd2
Salmaaattia <you@example.com>
2021-02-22 18:59:04 +0200

    change url pattern for registeration

--- socialplatform/main_app/urls.py
+++ socialplatform/main_app/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('register', views.register, name='home-register'),
+    path('register', views.register, name='register'),
     path('login', views.login, name='login'),
     path('logout', views.logout, name='logout'),
     path('post', views.post, name='post')
--- socialplatform/socialplatform/urls.py
+++ socialplatform/socialplatform/urls.py
@@ -21,5 +21,5 @@ from users import views as user_views
 urlpatterns = [
     path('main_app/', include('main_app.urls')),
     path('admin/', admin.site.urls),
-    path('register/', user_views.register, name='register')
+    path('register/', user_views.register, name='home-register')
 ]
