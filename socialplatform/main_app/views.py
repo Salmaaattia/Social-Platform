aee2a016108db21d36b5f20f9b083a7436403eaa
Salmaaattia <you@example.com>
2021-02-22 21:49:54 +0200

    remove irrelevant views

--- socialplatform/main_app/views.py
+++ socialplatform/main_app/views.py
@@ -7,18 +7,5 @@ from .models import Post
 def home(request):
     return render(request, 'main_app/home.html', {'posts': Post.objects.all()})
 
-
-def register(request):
-    return render(request, 'main_app/register.html')
-
-
-def login(request):
-    return HttpResponse("please login")
-
-
-def logout(request):
-    return HttpResponse("logout")
-
-
 def post(request):
-    return HttpResponse("post")
+    return render(request, 'main_app/post.html', {'posts': Post.objects.all()})
