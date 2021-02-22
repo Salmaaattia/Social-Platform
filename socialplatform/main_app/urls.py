3a55917af39afda1b3d8c95c0d1db763e00cb78a
Salmaaattia <you@example.com>
2021-02-22 21:49:39 +0200

    remove irrelevant paths

--- socialplatform/main_app/urls.py
+++ socialplatform/main_app/urls.py
@@ -4,8 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('register', views.register, name='register'),
-    path('login', views.login, name='login'),
-    path('logout', views.logout, name='logout'),
-    path('post', views.post, name='post')
+    path('post', views.post, name='post'),
 ]
